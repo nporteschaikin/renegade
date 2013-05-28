969b29e76db94b3ca3c08bc44af0d1a7ee45f2a4
Noah Portes Chaikin <noah@kingandpartners.com>
2013-05-28 17:32:04 -0400

    added rescue for bad video request

--- app/models/items/links/video.rb
+++ app/models/items/links/video.rb
@@ -5,8 +5,17 @@ module Items
 			require 'open-uri'
 			attr_accessor :json
 			validates :vid, presence: true
+			validate :is_valid_response
 			has_attached_file :cover
 			has_attached_file :profile_image
+			
+			def is_valid_response
+				begin
+					puts self.response
+				rescue OpenURI::HTTPError
+					errors.add(:vid, "is invalid")
+				end
+			end
 			
 			def response
 				self.json = self.json || JSON.parse(open(api.gsub("*", self.vid.to_s)).read)
