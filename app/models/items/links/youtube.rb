3b2adc87cd0517900fa3e20b478a51ff74825030
Noah Portes Chaikin <noah@kingandpartners.com>
2013-05-28 17:19:17 -0400

    youtube model

--- app/models/items/links/youtube.rb
+++ app/models/items/links/youtube.rb
@@ -14,6 +14,13 @@ module Items
 				/((\w|-){11})(?:\S+)?/
 			end
 			
+			before_save do
+				self.name = self.response["data"]["title"]
+				self.author = self.response["data"]["uploader"]
+				self.description = self.response["data"]["description"]
+				self.cover = URI.parse(self.response["data"]["thumbnail"]["hqDefault"])
+			end
+			
 		end
 	end
 end
