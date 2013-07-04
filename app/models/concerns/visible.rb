2ac4016efc12b456e8a41126d78565177780c618
Noah Portes Chaikin <noah@porteschaikin.com>
2013-07-04 11:40:51 -0400

    this is sick. now you can search by location

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -14,10 +14,37 @@ module Visible
 			end
 		end
 		
-		def self.popular(time, limit = false)
-			return false unless time.instance_of? Float
-			ids = Visitor.where(visited_type: self.name, created_at: time.ago..Time.now).count(group: :visited_id, order: "count_all DESC").collect { |x| x.first }
-			self.where(id: limit ? ids.take(limit) : ids).order("FIELD(id,#{ids.join(',')})")
+		def self.popular(arguments)
+			
+			# set parent class
+			visitors = Visitor.where(visited_type: self.name)
+			
+			# if location is set
+			if arguments[:location]
+				arguments[:distance] = 20 unless arguments[:distance]
+				visitors = visitors.near(arguments[:location], arguments[:distance], order: false)
+			end
+			
+			# if time is set
+			if arguments[:time]
+				return false unless arguments[:time].instance_of?(Fixnum) || arguments[:time].instance_of?(Float)
+				visitors = visitors.where(created_at: arguments[:time].ago..Time.now)
+			end
+			
+			# count and order
+			count = visitors.count(group: :visited_id, order: "count_all DESC")
+			
+			if count.any?
+				# get all ids
+				ids = count.collect { |x| x.first }
+				# if limit is set
+				ids = ids.take(arguments[:limit]) if arguments[:limit]
+				# return!
+				self.where(id: ids).order("FIELD(id,#{ids.join(',')})")
+			else
+				return false
+			end
+			
 		end
 		
 	end
