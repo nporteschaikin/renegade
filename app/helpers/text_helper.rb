d7e710ff0d5c405369422019d7d83776f873f8f2
Noah Portes Chaikin <noah@porteschaikin.com>
2013-07-06 16:10:56 -0400

    fixed tag helper

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -1,7 +1,7 @@
 module TextHelper
 	
 	def tagged(string)
-		raw(string.gsub(/(?:\s|^)(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/i) { link_to("##{$1}", tag_path("#{$1}")) })
+		raw(string.gsub(/(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/) { link_to("##{$1}", tag_path("#{$1}")) })
 	end
 	
 end
