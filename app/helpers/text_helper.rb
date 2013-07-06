1f256ab542e128a6117c88f2fd89bd5f836ccac7
Noah Portes Chaikin <noah@porteschaikin.com>
2013-07-06 16:27:47 -0400

    tweet parsing

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -4,4 +4,9 @@ module TextHelper
 		raw(string.gsub(/(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/) { link_to("##{$1}", tag_path("#{$1}")) })
 	end
 	
+	def tweeted(string)
+		raw(string.gsub(/(http\:\/\/[A-Za-z0-9\/\.\?\=\-]*)/,'<a target="_blank" href="\1">\1</a>')
+			.gsub(/@(\w+)/, %Q{<a target="_blank" href="http://twitter.com/\\1">@\\1</a>}))
+	end
+	
 end
