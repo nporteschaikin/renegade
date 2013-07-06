07783c25afd443e40e1ed9a9fe3e4c68f802cafc
Noah Portes Chaikin <noah@porteschaikin.com>
2013-07-05 20:11:02 -0400

    final deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :repository,  "git@github.com:nporteschaikin/renegade.git"
 set :user, "deploy"
 set :deploy_to, "/apps/renegade"
 set :ssh_options, { :forward_agent => true }
+set :puma_socket, "tcp://127.0.0.1:3000"
 
 role :web, "205.186.136.165"
 role :app, "205.186.136.165"
