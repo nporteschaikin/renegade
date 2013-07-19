50cc5b69ff95ddd253e4c340fd6a2ae3c07316be
Noah Portes Chaikin <noah@porteschaikin.com>
2013-07-19 08:50:11 -0400

    miss u assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ namespace :deploy do
 		run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
 	end
 	task :start, except: { no_release: true } do
-		run "cd #{current_path} ; bundle exec unicorn_rails -p 5000 -D -E production"
+		run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D -E production"
 	end
 	task :stop, except: { no_release: true } do
 		run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
